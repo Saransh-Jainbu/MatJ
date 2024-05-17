a97184b320d8dbf9927038927e36e1401681c8bd
Saransh Jain <saranshj647@gmail.com>
2024-05-17 17:22:55 +0530

    Navigation Menu [Mobile/Desktop]

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#808080'}
+   },
+   fontFamily: {
+    'display':['Poppins','sans-serif'],
+    'body':['Inter','sans-serif']
+   }
+  },
+  plugins: [],
+}
